eef6eb27590028786a1d8e5b4e798e8559110aab
shinmike <shinmike90@gmail.com>
2017-03-14 15:55:55 +0000

    step 6 complete

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require('fs');
 
 var GITHUB_USER = "shinmike";
 var GITHUB_TOKEN = "725efd6b8265de3b8377392137d5614c9a57d552";
@@ -7,8 +8,32 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
-  console.log(requestURL);
-}
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request.get(options, function (error, response, body)  {
+
+     if(!error && response.statusCode == 200) {
+       var data = JSON.parse(body);
+
+       var result = data.map(function(value) {
+         console.log(value.avatar_url);
+       })
+
+       if (error){
+         console.log('Error, status:', response.statusCode)
+       }
+
+       if (response !== 200) {
+         console.log ('Error with status:', response.statusCode)
+       }
+       }
+  });
+};
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
