ca22d8d3b1a1173ed52cda53cde649426bd2739f
shinmike <shinmike90@gmail.com>
2017-03-14 03:22:51 +0000

    Step 4: Create the requestURL

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,16 @@
 var request = require('request');
 
+var GITHUB_USER = "shinmike";
+var GITHUB_TOKEN = "725efd6b8265de3b8377392137d5614c9a57d552";
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
 }
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
