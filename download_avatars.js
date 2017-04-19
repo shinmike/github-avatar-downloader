b9968223f7f75ce45d90ca11749bd0b77a4cd88b
shinmike <shinmike90@gmail.com>
2017-04-18 17:06:28 -0700

    Update download_avatars.js
    Revised solution with comments

--- download_avatars.js
+++ download_avatars.js
@@ -2,7 +2,7 @@ var request = require('request');
 var fs = require('fs');
 
 var GITHUB_USER = "shinmike";
-var GITHUB_TOKEN = "725efd6b8265de3b8377392137d5614c9a57d552";
+var GITHUB_TOKEN = "29e352f40b5364b8f86cc54a776ea09d349da289";
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -12,8 +12,7 @@ var repoName = process.argv[3];
 function getRepoContributors(repoOwner, repoName, cb) {
 
   if (!repoOwner || !repoName) {
-   console.error('Please return correct name and repo')
-   return;
+    console.error('Please return correct name and repo');
   }
 
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
@@ -25,39 +24,115 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
-  request.get(options, function (error, response, body)  {
-
-    if(!error && response.statusCode == 200) {
-      var data = JSON.parse(body);
-      if (error){
-        console.log('Error, status:', response.statusCode);
-      };
-      if (response !== 200) {
-       console.log ('Correct outcome. Status code is:', response.statusCode);
-     };
-      cb(null, result)
+  function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var info = JSON.parse(body);
+      // var arr = [];
+      // info.forEach(function(value){
+      //   arr.push(value.avatar_url);
+      // });
+      cb(null, info);
     }
-  });
+  }
+
+  // Step 5 - not clear how this is called?...
+  request(options, callback);
+
 }
 
 getRepoContributors(repoOwner, repoName, function(err, result) {
- if(!err) {
-   for(one of result) {
-     console.log(one.login + "   :   " + one.avatar_url);
-     const origFileName = one.login;
-     const newFileName = `./${origFileName}.jpg`;
-     downloadImageByURL(one.avatar_url, newFileName);
-   }
- }
+  if (!err){
+    for (var i = 0; i < result.length; i++){
+      var origFileName = result[i].login;
+      var newFileName = "avatars/" + origFileName + ".jpg";
+      downloadImageByURL(result[i].avatar_url, newFileName);
+    }
+  }
 });
 
 function downloadImageByURL(url, filePath) {
- request.get(url)
- .on('error', function (err) {
-   throw err;
- })
- .on('response', function (response) {
-   console.log('Response Status Code: ', response.statusCode);
-})
- .pipe(fs.createWriteStream(filePath));
+  console.log("Downloading image...");
+  var stream = request.get(url)
+  .on('error', function (err) {
+    throw err;
+  })
+  .on('response', function (response) {
+    console.log('Downloading image status: ', response.statusCode);
+  })
+  .pipe(fs.createWriteStream(filePath));
+  stream.on('finish', function(){
+     console.log("Downloading image complete.");
+  });
 }
+
+
+
+
+
+
+
+// ------------------------ ORIGINAL SOLUTION
+
+// var request = require('request');
+// var fs = require('fs');
+//
+// var GITHUB_USER = "shinmike";
+// var GITHUB_TOKEN = "29e352f40b5364b8f86cc54a776ea09d349da289";
+//
+// console.log('Welcome to the GitHub Avatar Downloader!');
+//
+// var repoOwner = process.argv[2];
+// var repoName = process.argv[3];
+//
+// function getRepoContributors(repoOwner, repoName, cb) {
+//
+//   if (!repoOwner || !repoName) {
+//    console.error('Please return correct name and repo');
+//    return;
+//   }
+//
+//   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+//
+//   var options = {
+//     url: requestURL,
+//     headers: {
+//       'User-Agent': 'request'
+//     }
+//   };
+//
+//   request.get(options, function (error, response, body)  {
+//
+//     if(!error && response.statusCode == 200) {
+//       var data = JSON.parse(body);
+//       if (error){
+//         console.log('Error, status:', response.statusCode);
+//       }
+//       if (response !== 200) {
+//        console.log ('Correct outcome. Status code is:', response.statusCode);
+//      }
+//       cb(null, result);
+//     }
+//   });
+// }
+//
+// getRepoContributors(repoOwner, repoName, function(err, result) {
+//  if(!err) {
+//    for(one of result) {
+//      console.log(one.login + "   :   " + one.avatar_url);
+//      const origFileName = one.login;
+//      const newFileName = `./${origFileName}.jpg`;
+//      downloadImageByURL(one.avatar_url, newFileName);
+//    }
+//  }
+// });
+//
+// function downloadImageByURL(url, filePath) {
+//  request.get(url)
+//  .on('error', function (err) {
+//    throw err;
+//  })
+//  .on('response', function (response) {
+//    console.log('Response Status Code: ', response.statusCode);
+// })
+//  .pipe(fs.createWriteStream(filePath));
+// }
